5dabe4964ac2f51c687ba4719b740b58e86ff7e7
parveen <mittalparveen652@gmail.com>
2020-03-24 12:44:55 +0530

    working tested

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,8 +1,8 @@
 public class  HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
-
-        for (int j = 0; i < 1000; i=4+i, j=j+4) {
+        int j=0;
+        for (; i < 1000; i=4+i, j=j+4) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
